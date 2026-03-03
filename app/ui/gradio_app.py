062e35d7b5d2359a3e56a1189adcdc758648bc9f
a4amk <abdulmannankhan95@gmail.com>
2026-03-03 21:07:40 +0500

    docs: Update README screenshot and remove exclamation mark from default text input.

--- app/ui/gradio_app.py
+++ app/ui/gradio_app.py
@@ -239,7 +239,7 @@ def create_blocks():
             with gr.Column(scale=5):
                 text_input = gr.Textbox(
                     label="Text to Synthesize", lines=12, 
-                    value="I need to present the present to the lead singer, who is currently holding a lead pipe. Don't desert me in the desert; I haven't finished my dessert yet.!"
+                    value="I need to present the present to the lead singer, who is currently holding a lead pipe. Don't desert me in the desert; I haven't finished my dessert yet."
                 )
                 with gr.Accordion("Chunking Rules (Regex Patterns)", open=True):
                     split_choice_input = gr.Dropdown(
